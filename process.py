72d2f216993832507db18bd8bd56b8c94ec11150
Luis <lab156@case.edu>
2018-07-17 13:05:48 -0400

    untar tar subfiles

--- process.py
+++ process.py
@@ -26,6 +26,15 @@ def tar2api(id_str):
     '''
     name = re.match(r'.*([0-9]{4})\.([0-9]{5})',id_str)
     return name.group(1) + '.' + name.group(2) 
+
+def tar_id(id_str):
+    '''
+    Given a path like: /mnt/arXiv_src/src/arXiv_src_1804_001.tar
+    return the unique identifier 1804_001
+    '''
+    name = re.match(r'.*arXiv_src_([0-9]{4})_([0-9]{3})\.tar', id_str)
+    return name.group(1) + '_' + name.group(2)
+
     
 class Xtraction(object):
     def __init__(self, tar_path, *argv, **kwarg):
@@ -54,20 +63,54 @@ class Xtraction(object):
         else:
             return [d for d in self.query_results if d['tags'][0]['term']==MSC]
 
-    def extract_dir(self, filename, output_dir):
+    def path_dir(self, output_dir):
         '''
-        given filename, extract a file with that name from
+        Create if not exists dir with path:
+        output_dir
+          |_
+            1804.0001   (tar_id name)
+        '''
+        the_path = os.path.join(output_dir, tar_id(self.tar_path))
+        if not os.path.exists(the_path):
+            os.makedirs(the_path)
+        return the_path
+
+
+    def extract_any(self, filename, output_dir):
+        '''
+        given filename, decide it is a tape archive (tar)
+        or gzipped compressed file and extract it to the directory output_dir
         self.tar
+        Example
+        filename: /mnt/arXiv_src/src/arXiv_src_1804_001.tar
+        output_dir: math.DG
         '''
+        short_name = tar2api(filename) # format 
+        output_path = os.path.join(self.path_dir(output_dir), short_name)
+        os.mkdir(output_path)
         with tarfile.open(self.tar_path) as ff:
             file_gz = ff.extractfile(filename)
-            if tarfile.is_tarfile(file_gz):
-                with tarfile.open(file_gz) as ftar: 
-                    ftar.extractall(filename)
-            else:
-                with gzip.open(file_gz,'rb') as fgz:
-                    file_str = fgz.read()
-        return file_str.decode('utf-8')
+            #if tarfile.is_tarfile(file_gz):
+#                with tarfile.open(file_gz) as ftar: 
+#                    ftar.extractall(output_path)
+#            else:
+            with gzip.open(file_gz,'rb') as fgz:
+                file_str = fgz.read()
+            try:
+                decoded_str = file_str.decode('utf-8')
+                with open(os.path.join(output_path, short_name + '.tex'),'w')\
+                        as fname:
+                    fname.write(decoded_str)
+            except UnicodeDecodeError:
+                # file_gz is not zipped so try to untar the file instead
+                print('attempting to write %s to %s'%(short_name,output_path))
+                with tarfile.open(self.tar_path) as ff:
+                    tar2 = ff.extractfile(filename)
+                    with tarfile.open(fileobj=tar2) as tars:
+                        tars.extractall(path=output_path)
+            # the file 1804/1804.01815.gz in 1804_005 has something different
+
+        return True
 
     def extract_str(self, filename):
         '''
@@ -86,9 +129,7 @@ if __name__ == '__main__':
     f_lst = x.filter_MSC('math.DG')
     for f in f_lst:
         print('writing file %s'%f)
-        with open(os.path.join(sys.argv[2],tar2api(f) + '.tex'), 'w') as fname:
-            fname.write(x.extract_dir(f))
+        x.extract_any(f, sys.argv[2])
+        #with open(os.path.join(sys.argv[2],tar2api(f) + '.tex'), 'w') as fname:
+        #    fname.write(x.extract_dir(f))
     
-
-
-
