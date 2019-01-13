bc39691f13a9ea5b78c849f33deb346765f10bc0
Luis <lab156@case.edu>
2019-01-13 13:10:39 -0500

    run_latexml.sh runs on shell variable expansions

--- README.md
+++ README.md
@@ -12,6 +12,17 @@ TEXF=`ls *.tex`; latexml $TEXF.tex 2>&1 > ${TEXF%.*}.xml | tail -15 >> commentar
 find ./* -maxdepth 0 -type d '!' -exec test -e "{}/latexml_errors_mess.txt" ';' -print
 ```
 
+* To filter manually cancelled latexml processes search in the latex_errors file with:
+```
+Fatal:perl:die Perl died
+```
+
+* When LaTeXML runs out of memory for example in 1504.06138
+```
+(Processing definitions /usOut of memory!
+```
+
+
 ### Notes
 * There is a limit of around 500 articles id that the API can handle.
 * In 2014 the article name format changed from YYMM.{4 digits} to 5 digits.
--- run_latexml.sh
+++ run_latexml.sh
@@ -1,26 +1,18 @@
 #! /bin/bash
 
-# Run latexml on all the .tex files under argv[1] 
+# Run latexml on  the main .tex files under argv[1] 
 # Sends the Errors to the files latexml_errors_mess.txt
 # and the processed file is stored in an xml file with 
 #the same name as the original .tex file.
-
-FILE_LIST=($(find $1 -iname *.tex))
 #FILE_LIST=($(find  $1 -maxdepth 1 -type d '!' -exec test -e "{}/latexml_errors_mess.txt" ';' -print))
 
-#for f in ${FILE_LIST[@]}; do
-#     echo processing the file $f;
-#    xpath=${f%.*}
-#    echo $xpath
-#    xbase=${f##*/}
-#    xfext=${xbase##*.}
-#    echo xbase is: $xbase
-#    echo xfext is: $xfext
-#    echo see if this works ${f%/*}
-     #latexml $f  2>&1 > ${f%.*}.xml | echo  > ${f%/*}/latexml_errors_mess.txt
-f=$(perl get_main_tex.pl $1)
-echo $1 $f ${f%/*} ${f%.*}
-/home/luis/Paquetes/LaTeXML/bin/latexml $f  2>${f%/*}/latexml_errors_mess.txt > ${f%.*}.xml 
-#done
+for article_dir in "$@"
+do
+    f=$(perl get_main_tex.pl $article_dir)
+    echo "Running LaTeXML on the file " $f
+    COMMENTARY_FILE=${f%/*}/commentary.txt 
+    echo "main .tex file" $(basename $f) >> $COMMENTARY_FILE
+    /home/luis/Paquetes/LaTeXML/bin/latexml $f  2>${f%/*}/latexml_errors_mess.txt > ${f%.*}.xml 
+done
 
 
