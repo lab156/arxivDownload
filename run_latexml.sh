1db13404f0bac20301b1fb706a9c1216205f0bc8
Luis <lab156@case.edu>
2019-01-21 23:53:25 -0500

    add timeout functionality

--- run_latexml.sh
+++ run_latexml.sh
@@ -6,6 +6,13 @@
 #the same name as the original .tex file.
 #FILE_LIST=($(find  $1 -maxdepth 1 -type d '!' -exec test -e "{}/latexml_errors_mess.txt" ';' -print))
 
+#Define the LaTeXML binary file to run
+LATEXML_BIN=/home/luis/Paquetes/LaTeXML/bin/latexml 
+
+#The maximum amount of time in seconds that LaTeXML is
+#allowed to run (seconds)
+MAXT=1200
+
 for article_dir in "$@"
 do
     f=$(perl get_main_tex.pl $article_dir)
@@ -18,7 +25,13 @@ do
 	    echo "Running LaTeXML on the file " $f
 	    COMMENTARY_FILE=${f%/*}/commentary.txt 
 	    echo "main .tex file" $(basename $f) >> $COMMENTARY_FILE
-	    /home/luis/Paquetes/LaTeXML/bin/latexml $f  2>${f%/*}/latexml_errors_mess.txt > ${f%.*}.xml 
+	    timeout $MAXT  $LATEXML_BIN $f  2>${f%/*}/latexml_errors_mess.txt > ${f%.*}.xml 
+	    if [ $? -eq 124 ]; then
+		    echo "Timeout Occured with file $f"
+		    echo "Timeout of $MAXT seconds occured" >> $COMMENTARY_FILE
+	    else
+		    echo "Finished in less than $MAXT seconds" >> $COMMENTARY_FILE
+	    fi
     fi
 done
 
