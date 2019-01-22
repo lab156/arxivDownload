1ce634e14a3b8ad0fc34d08b1d498f050a43d26c
Luis <lab156@case.edu>
2019-01-21 22:35:08 -0500

    check if there is a main file

--- run_latexml.sh
+++ run_latexml.sh
@@ -9,10 +9,17 @@
 for article_dir in "$@"
 do
     f=$(perl get_main_tex.pl $article_dir)
-    echo "Running LaTeXML on the file " $f
-    COMMENTARY_FILE=${f%/*}/commentary.txt 
-    echo "main .tex file" $(basename $f) >> $COMMENTARY_FILE
-    /home/luis/Paquetes/LaTeXML/bin/latexml $f  2>${f%/*}/latexml_errors_mess.txt > ${f%.*}.xml 
+    if [ -z "$f" ]
+    then
+	    COMMENTARY_FILE=$article_dir/commentary.txt
+	    echo "Could not find the main tex file. $COMMENTARY_FILE"
+	    [ -f "$COMMENTARY_FILE" ] && echo "Main TeX file not found" >> $COMMENTARY_FILE
+    else
+	    echo "Running LaTeXML on the file " $f
+	    COMMENTARY_FILE=${f%/*}/commentary.txt 
+	    echo "main .tex file" $(basename $f) >> $COMMENTARY_FILE
+	    /home/luis/Paquetes/LaTeXML/bin/latexml $f  2>${f%/*}/latexml_errors_mess.txt > ${f%.*}.xml 
+    fi
 done
 
 
