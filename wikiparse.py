f52ce504e51f34e98c95d721c4063ecd8b24aa6a
Luis <lab156@case.edu>
2019-02-27 02:02:00 -0500

    wikiparse works and stores definitions

--- wikiparse.py
+++ wikiparse.py
@@ -2,7 +2,7 @@ from lxml import etree
 from bz2 import BZ2File
 import re
 
-def fast_iter(xml_file, sentinel=None):
+def fast_iter(xml_file, out_file=None, sentinel=None):
     '''
     xml_file : is a readable file object
     sentinel : is an int of the max number of pages to check
@@ -10,15 +10,20 @@ def fast_iter(xml_file, sentinel=None):
     ns = '{http://www.mediawiki.org/xml/export-0.10/}'
     nsp = {'wiki':'http://www.mediawiki.org/xml/export-0.10/'}
     parser = etree.iterparse(xml_file, tag=ns+'page')
-    reg_expr = re.compile('== .*definition ==', re.I)
+    reg_expr = re.compile('=+ ([\w ]*definition) =+\n(.+?)\n\n', re.I|re.DOTALL)
     senti = 0
     for event,elem in parser:
         text = elem.find('wiki:revision/wiki:text', namespaces=nsp)
         title = elem.find(ns+'title')
-        if '== Definition ==' in text.text:
-            print(title.text)
+        regex_def = re.findall(reg_expr, text.text)
+        if regex_def:
+            if title.text.lower() in regex_def[0][1].lower():
+                print('\033[1m' + title.text + '\033[0m', ' --- ', regex_def[0][0], ' --- ', regex_def[0][1])
+                if out_file:
+                    print(title.text, ' --- ', regex_def[0][0], ' --- ',
+                            repr(regex_def[0][1]), file=out_file)
         elem.clear()
-         text.clear()
+        text.clear()
         title.clear()
         while elem.getprevious() is not None:
             del elem.getparent()[0]
@@ -34,10 +39,21 @@ def fast_iter(xml_file, sentinel=None):
 
 
 if __name__ == '__main__':
+    '''
+    Usage:
+        python wikiparse.py ../enwiki-20190201-pages-articles-multistream1.xml-p10p30302.bz2 -s 650
+python wikiparse.py ../enwiki-20190201-pages-articles-multistream1.xml-p10p30302.bz2 -s 1500 -o ../test_def.txt
+    '''
+
     import argparse
     parser = argparse.ArgumentParser( description='Parse the wikipedia dump file')
     parser.add_argument('path', help='wikipedia .bz2 file')
     parser.add_argument('-s', '--sentinel', help='file to write the logs', default=None)
+    parser.add_argument('-o', '--outfile', help='file to write the definitions', default=None)
     args = parser.parse_args()
     with BZ2File(args.path) as xml_file:
-        fast_iter(xml_file, sentinel=int(args.sentinel))
+        if args.outfile:
+            with open(args.outfile, 'a') as out_file_f:
+                fast_iter(xml_file, out_file=out_file_f, sentinel=int(args.sentinel))
+        else:
+            fast_iter(xml_file, sentinel=int(args.sentinel))
