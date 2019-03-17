5821185e91b798e6d2931b381473ecc5e24930cc
Luis <lab156@case.edu>
2019-03-17 01:58:50 -0400

    this is the right change that does run on all wikipedia file

--- wikiparse.py
+++ wikiparse.py
@@ -1,6 +1,7 @@
 from lxml import etree
 from bz2 import BZ2File
 import re
+from unwiki import unwiki
 
 reg_expr = re.compile('(?P<nequals>=+)([\w\s]*?definition) *(?P=nequals)\n(.+?)\n\n=+', re.I|re.DOTALL)
 nsp = {'wiki':'http://www.mediawiki.org/xml/export-0.10/'}
@@ -41,34 +42,28 @@ def fast_iter(xml_file, out_file=None, sentinel=None):
     xml_file : is a readable file object
     sentinel : is an int of the max number of pages to check
     '''
-    ns = '{http://www.mediawiki.org/xml/export-0.10/}'
-    nsp = {'wiki':'http://www.mediawiki.org/xml/export-0.10/'}
+    sep_string = ' -#-%- '
     parser = etree.iterparse(xml_file, tag=ns+'page')
-    reg_expr = re.compile('=+ ([\w ]*definition) =+\n(.+?)\n\n', re.I|re.DOTALL)
     senti = 0
     for event,elem in parser:
-        text = elem.find('wiki:revision/wiki:text', namespaces=nsp)
-        title = elem.find(ns+'title')
-        try:
-            regex_def = re.findall(reg_expr, text.text)
-        except TypeError:
-            print('Article with title %s has no text :('%title.text)
-        if regex_def:
-            if title.text.lower() in regex_def[0][1].lower():
-                print('\033[1m' + title.text + '\033[0m', ' --- ', regex_def[0][0], ' --- ', regex_def[0][1])
+        res_lst = search_defs(elem, namespace=nsp)
+        for r in res_lst:
+            if r['matches']:
+                print('\033[1m' + r['title'] + '\033[0m',
+                sep_string , r['section'], sep_string, r['definition'])
                 if out_file:
                     print(r['title'] ,
                 sep_string , r['section'], 
                 sep_string, r['definition'], file=out_file)
         elem.clear()
-        text.clear()
-        title.clear()
+        #text.clear()
+        #title.clear()
         while elem.getprevious() is not None:
             del elem.getparent()[0]
-        while text.getprevious() is not None:
-            del text.getparent()[0]
-        while title.getprevious() is not None:
-            del title.getparent()[0]
+        #while text.getprevious() is not None:
+        #    del text.getparent()[0]
+        #while title.getprevious() is not None:
+        #    del title.getparent()[0]
 
         if sentinel > 0:
             if senti > sentinel:
@@ -84,7 +79,8 @@ python wikiparse.py ../enwiki-20190201-pages-articles-multistream1.xml-p10p30302
     '''
 
     import argparse
-    parser = argparse.ArgumentParser( description='Parse the wikipedia dump file')
+    parser = argparse.ArgumentParser(
+            description='Parse the wikipedia dump file')
     parser.add_argument('path', help='wikipedia .bz2 file')
     parser.add_argument('-s', '--sentinel',
             help='file to write the logs', default=0)
@@ -94,6 +90,7 @@ python wikiparse.py ../enwiki-20190201-pages-articles-multistream1.xml-p10p30302
     with BZ2File(args.path) as xml_file:
         if args.outfile:
             with open(args.outfile, 'a') as out_file_f:
-                fast_iter(xml_file, out_file=out_file_f, sentinel=int(args.sentinel))
+                fast_iter(xml_file, out_file=out_file_f,
+                        sentinel=int(args.sentinel))
         else:
             fast_iter(xml_file, sentinel=int(args.sentinel))
