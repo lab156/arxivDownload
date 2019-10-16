eeacffca0a788b994552f28b8a7aa9816cffc929
Luis <lab156@case.edu>
2019-10-16 01:44:33 -0400

    recover parsing_manifest function (don't know how it went missing), and add __main__ method to dload.py

--- dload.py
+++ dload.py
@@ -3,6 +3,35 @@ import subprocess
 import hashlib
 import xml.etree.ElementTree as ET
 import pandas as pd
+import sys
+import logging
+
+
+def parse_element(elem):
+    return_dict = {}
+    for e in elem:
+        return_dict[e.tag] = e.text # loop element and extract info
+    return return_dict
+def parse_root(root):
+        return [parse_element(child) for child in iter(root) \
+                if child.tag != 'timestamp']
+
+def parse_manifest(xml_path):
+    '''
+    xml_path: the path of the xml manifest file
+    output: a pandas dataframe of all the files in the src bucket
+
+    ### If you want to create an allfiles.csv ###
+    1) Download the manifest from aws: $s3cmd get --requester-pays s3://arxiv/src/arXiv_src_manifest.xml
+    2) On the jupyter notebook: Parsing arxiv manifest and querying metadata do
+    new_all = parse_manifest(FilePath.xml)
+    new_all.to_csv(FilePath.csv)
+    '''
+    with open(xml_path, 'r') as f:
+        mani = ET.parse(f)
+    root = mani.getroot()
+    return pd.DataFrame(parse_root(root))
+
 
 class DownloadMan(object):
     def __init__(self,
@@ -35,7 +64,11 @@ class DownloadMan(object):
         filename string is an attribute
         '''
         idx = self.allfiles_df.filename.isin(self.downloaded_df.filename)
-        return self.allfiles_df[~idx].iloc[0]
+        try:
+            return self.allfiles_df[~idx].iloc[0]
+        except IndexError as e:
+            logging.info("No next_file found")
+            raise e
 
     def get(self, filename):
         file_save_path = os.path.join(self.mountpoint, filename)
@@ -89,3 +122,20 @@ class DownloadMan(object):
         return int(return_size)
 
 
+if __name__ == '__main__':
+    ## Default values
+    mountpoint = '/mnt/arXiv_src/'
+    allfiles = 'allfiles2.csv'
+    doun = 'downloaded_log.csv'
+    error_log = 'error_dload.log'
+    logging.basicConfig(filename='../error_log_dload.log', 
+            filemode='w',
+            level=logging.DEBUG,
+            format='%(asctime)s - %(message)s')
+    D = DownloadMan(mountpoint, allfiles, doun, error_log)
+    while True:
+        try:
+            D.get_next()
+        except IndexError:
+            break
+    sys.exit(0)
