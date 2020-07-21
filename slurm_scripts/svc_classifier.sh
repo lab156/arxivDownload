4b8141db2e83c9cbccb512e1ad9d2349676c132d
Luis Berlioz <lab232@br005.pvt.bridges.psc.edu>
2020-07-21 12:53:20 -0400

    final slurm script that worked

--- slurm_scripts/svc_classifier.sh
+++ slurm_scripts/svc_classifier.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --time=0-08:00:00
+#SBATCH --time=0-40:00:00
 #SBATCH --job-name=svc_train
 #SBATCH --output=svc_train.log
 #SBATCH --mail-user=lab232@pitt.edu #send email to this address if ...
 #SBATCH --mail-type=END,FAIL # ... job ends or fails
 #SBATCH --nodes=1
 #SBATCH --partition=LM
-#SBATCH --mem=250GB
+#SBATCH --mem=128GB
 #SBATCH --account=trz3aap
 
 
