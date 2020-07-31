85f1ddb9fb1038e37971b5e807fa281ef3ba688e
Luis <lab156@case.edu>
2020-07-30 22:28:39 -0400

    Thu 30 Jul 2020 10:28:39 PM EDT

--- embed/Word embeddings generation and evaluation.py
+++ embed/Word embeddings generation and evaluation.py
@@ -36,6 +36,7 @@ import re
 import struct as st
 from itertools import islice
 import numpy as np
+from yellowbrick.text import TSNEVisualizer
 
 # %load_ext autoreload
 # %autoreload 2
@@ -43,6 +44,7 @@ from report_lengths import generate
 # -
 
 ------- Do not run -----
+# This strips all the text from the xml articles and saves to text file
 for art_path in tqdm(glob.glob('/mnt/promath/math15/*.tar.gz')):
     art_str = ""
     for name, fobj in peep.tar_iter(art_path, '.xml'):
@@ -62,29 +64,47 @@ for art_path in tqdm(glob.glob('/mnt/promath/math15/*.tar.gz')):
 # Connect to the database
 #database = 'sqlite:///../../arxiv2.db'
 #eng = sa.create_engine(database, echo=False)
-#eng.connect()
+#eng.connect().execute('pragma case_sensitive_like=OFF;')
 #SMaker = sessionmaker(bind=eng)
 #sess = SMaker()
 
 # Get tarfile set of names
 def qq(art_str):
     q = sess.query(cre.Article)
-    res = q.filter(cre.Article.id.like("%"+ art_str + "%")).first()
+    res = q.filter(cre.Article.id.like("http://arxiv.org/abs/"+ art_str + "%")).first()
     try:
         lst = eval(res.tags)[0]['term']
     except AttributeError:
         lst = None
-    #print(res.id)
+    print(res.id)
     return lst
 qq('1512.09109')
+
+
 # -
+
+# %%time
+def qq(art_str, database = 'sqlite:///../../arxiv2_test_ind.db'):
+    # Connect to the database
+    eng = sa.create_engine(database, echo=False)
+    with eng.connect() as con:
+        con.execute('pragma case_sensitive_like=OFF;')
+        #resu = con.execute('select tags from articles where id like "http://arxiv.org/abs/1512.09109%;"')
+        #resu = con.execute('select tags from articles where id glob "http://arxiv.org/abs/1512.09109*";')
+        q_str = '''select tags from articles where id 
+        between "http://arxiv.org/abs/{0}" and "http://arxiv.org/abs/{0}{{";'''.format(art_str)
+        resu = next(con.execute(q_str))
+    #resu format: ("[{'term': 'math.RT', 'scheme': 'http://arxiv.org/schemas/atom', 'label': None}]",)
+    res_dict = eval(resu[0])
+    return res_dict[0]['term']
+qq('1512.09109')    
 
 ## Grab the Glossary data
 dfndum_set = set()
 new_dfndum_lst = [0]
 tot_dfndum_lst = [0]
 rep_ratio = []
-#term_cnt = Counter()
+term_cnt = Counter()
 term_dict_cnt = defaultdict(Counter)
 perc_array = np.array([])
 for xml_path in tqdm(glob.glob('/mnt/glossary/v1.1/math15/*.xml.gz')):
@@ -92,16 +112,23 @@ for xml_path in tqdm(glob.glob('/mnt/glossary/v1.1/math15/*.xml.gz')):
     for art in gtree.iter(tag='article'):
         d_lst = [d.text for d in art.findall('.//dfndum')]
         dfndum_set.update(d_lst)
-        #term_cnt.update(d_lst)
+        term_cnt.update(d_lst)
         new_dfndum_lst.append(len(dfndum_set))
         tot_dfndum_lst.append(tot_dfndum_lst[-1] + len(d_lst))
         rep_ratio.append(tot_dfndum_lst[-1]/len(dfndum_set))
+        arxiv_class = qq(art.attrib['name'].split('/')[1])
+        #print(f"Found arxiv class {arxiv_class}")
         for D in d_lst:
-            arxiv_class = qq(art.attrib['name'].split('/')[1])
-            term_dict_cnt[D].update([qq(arxiv_class)])
+            term_dict_cnt[D].update([arxiv_class])
 
-s = 1005
-term_cnt.most_common()[s:s+10]
+# + jupyter={"outputs_hidden": true}
+term_cnt.most_common()[25:]
+# -
+
+s = 3002
+Term = term_cnt.most_common()[s]
+#print(f'The term: {Term} appears in articles tagged:')
+term_dict_cnt['weight vector']
 
 # Decode word2vec .bin file
 with open('../../word2vec/math15-vectors-phrase.bin', 'rb') as mfobj:
@@ -133,10 +160,49 @@ with open('../../word2vec/math15-vectors-phrase.bin', 'rb') as mfobj:
         cnt +=1
         embed[word] = vec
 
-common_term = term_cnt.most_common()[100][0].lower().replace(' ', '_')
+common_term = term_cnt.most_common()[2001][0].lower().replace(' ', '_')
 print(f" The term is: {common_term}")
 embed.get(common_term, None)[:10]
 
-list(filter( lambda s: 'complete' in s, embed.keys()))
+# Create a dict of "very AG" vectors
+veryAG = {}
+veryDG = {}
+for Term_pair in tqdm(term_cnt.most_common()):
+    Term = Term_pair[0]
+    if term_dict_cnt[Term]['math.AG'] > 4*term_dict_cnt[Term]['math.DG']:
+        embed_name = Term.lower().replace(' ', '_')
+        try:
+            embed_vec = embed.get(embed_name, None)
+            tot = float(term_dict_cnt[Term]['math.AG'] + term_dict_cnt[Term]['math.DG'])
+            color_intensity = (term_dict_cnt[Term]['math.AG']/tot, term_dict_cnt[Term]['math.DG']/tot)
+            if embed_vec is not None:
+                veryAG[Term] = (embed_vec, color_intensity)
+        except TypeError:
+            pass
+    if term_dict_cnt[Term]['math.DG'] > 4*term_dict_cnt[Term]['math.AG']:
+        embed_name = Term.lower().replace(' ', '_')
+        try:
+            embed_vec = embed.get(embed_name, None)
+            tot = float(term_dict_cnt[Term]['math.AG'] + term_dict_cnt[Term]['math.DG'])
+            color_intensity = (term_dict_cnt[Term]['math.AG']/tot, term_dict_cnt[Term]['math.DG']/tot)
+            if embed_vec is not None:
+                veryDG[Term] = (embed_vec, color_intensity)
+        except TypeError:
+            pass
+
+print(list(veryAG.keys())[:15])
+print(list(veryDG.keys())[:15])
+veryAG['graph']
+
+ag_vec = [v[0] for v in veryAG.values()]
+len(ag_vec[0])
+
+ag_lst = [v[0] for v in veryAG.values()][:500]
+dg_lst = [v[0] for v in veryDG.values()][:500]
+tot_vec = np.stack(ag_lst + dg_lst, axis=0)
+labels_vec = len(ag_lst)*['math.AG'] + len(dg_lst)*['math.DG']
+tsne2 = TSNEVisualizer(labels=['math.AG','math.DG'])
+tsne2.fit(tot_vec, labels_vec)
+tsne2.poof(figsize=100)
 
 
