13091974a5b702bee3eb3e4d138d41929b23ccea
Luis <lab156@case.edu>
2022-01-04 12:30:51 -0500

    Tue Jan  4 12:30:51 PM EST 2022

--- slurm_scripts/join_phrases/mpi_joiner.sh
+++ slurm_scripts/join_phrases/mpi_joiner.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --time=0-12:00:00
+#SBATCH --time=0-16:00:00
 #SBATCH --job-name=joining_phrases
 #SBATCH --output=log_phrases.txt
 #SBATCH --mail-user=lab232@pitt.edu #send email to this address if ...
 #SBATCH --mail-type=END,FAIL # ... job ends or fails
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --partition=RM
 
 module load python/3.8.6
