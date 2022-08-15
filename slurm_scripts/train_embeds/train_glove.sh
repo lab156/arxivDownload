8f4163fe10cefaf278dc17113e6a4aa6fd6d5b3a
Luis <lab156@case.edu>
2022-08-15 18:27:07 -0400

    Mon 15 Aug 2022 06:27:07 PM EDT

--- slurm_scripts/train_embeds/train_glove.sh
+++ slurm_scripts/train_embeds/train_glove.sh
@@ -1,68 +1,61 @@
 #!/bin/bash
-#SBATCH --time=0-20:00:00
-#SBATCH --job-name=arxiv_model
-#SBATCH --output=arxiv_model.log
+#SBATCH --time=0-05:00:00
+#SBATCH --job-name=train_glove
+#SBATCH --output=glv_%j.log
 #SBATCH --mail-user=lab232@pitt.edu #send email to this address if ...
 #SBATCH --mail-type=END,FAIL # ... job ends or fails
 #SBATCH --nodes=1
 #SBATCH --partition=RM
-#SBATCH --account=trz3akp
+#SBATCH --account=dms200016p
 
 
+# copy and modify set_local_var.sh file
+source gitignore_set_local_var
+SOURCE_DIR=`pwd`
 
-normalize_text() {
-  awk '{print tolower($0);}' | tr -cd '[:print:]' | sed -e "s/’/'/g" -e "s/′/'/g" -e "s/''/ /g" -e "s/'/ ' /g" -e "s/“/\"/g" -e "s/”/\"/g" \
-  -e 's/"/ " /g' -e 's/\./ \. /g' -e 's/<br \/>/ /g' -e 's/, / , /g' -e 's/(/ ( /g' -e 's/)/ ) /g' -e 's/\!/ \! /g' \
-  -e 's/\?/ \? /g' -e 's/\;/ /g' -e 's/\:/ /g' -e 's/-/ - /g' -e 's/=/ /g' -e 's/=/ /g' -e 's/*/ /g' -e 's/|/ /g' \
-  -e 's/«/ /g' | tr 0-9 " "
-}
-
-SCRATCH="/home/luis/Paquetes/GloVe"
-cd $SCRATCH
 #DATA="/pylon5/ms5pi8p/lab232/miniclean"
 #MODEL="/pylon5/ms5pi8p/lab232/model"`date '+%H-%M_%d-%m'`
 #DATA="/media/hd1/clean_text"
-DATA="/media/hd1/processed_text/joined_math20-02_24-01"
-MODEL="/home/luis/rm_me/model"`date '+%H-%M_%d-%m'`
+#DATA="/media/hd1/processed_text/joined_math20-02_24-01"
+#MODEL="/home/luis/rm_me/model"`date '+%H-%M_%d-%m'`
+
+MODEL=$MODELDIR/"glove_model_"`date '+%H-%M_%d-%m'`
 
 mkdir -p $MODEL
-for file in $DATA/math*;
-do
-#    normalize_text < $file >> $MODEL/data.txt
-    echo "adding $file to data"
-    cat $file >> $MODEL/data.txt
-done
+echo "Using the parameters:" | tee -a $MODEL/log.txt
+cat $SOURCE_DIR/gitignore_set_local_var | tee -a $MODEL/log.txt
 
-CORPUS=$MODEL/data.txt
-VOCAB_FILE=$MODEL/vocab.txt
-COOCCURRENCE_FILE=$MODEL/cooccurrence.bin
-COOCCURRENCE_SHUF_FILE=$MODEL/cooccurrence.shuf.bin
-BUILDDIR=$SCRATCH/build
-SAVE_FILE=$MODEL/vectors
-VERBOSE=2
-MEMORY=4.0
-VOCAB_MIN_COUNT=5
-VECTOR_SIZE=64
-MAX_ITER=20
-WINDOW_SIZE=15
-BINARY=2
-NUM_THREADS=8
-X_MAX=10
+
+#for file in $CLEANDATADIR/math*;
+#do
+#    echo "Concatenating file $file"
+#    cat $file | python3 run_normalize.py embed4classif >> $MODEL/data.txt
+#done
+# USE A CLEAN FILE DIRECTLY INSTEAD OF SPENDING TIME CLEANING math*
+echo "Using file $CLEANDATADIR/data.txt" | tee -a $MODEL/log.txt
+cp $CLEANDATADIR/data.txt $MODEL/data.txt
+
 if hash python 2>/dev/null; then
     PYTHON=python
 else
     PYTHON=python3
 fi
 
-echo
-echo "$ $BUILDDIR/vocab_count -min-count $VOCAB_MIN_COUNT -verbose $VERBOSE < $CORPUS > $VOCAB_FILE"
-$BUILDDIR/vocab_count -min-count $VOCAB_MIN_COUNT -verbose $VERBOSE < $CORPUS > $VOCAB_FILE
-echo "$ $BUILDDIR/cooccur -memory $MEMORY -vocab-file $VOCAB_FILE -verbose $VERBOSE -window-size $WINDOW_SIZE < $CORPUS > $COOCCURRENCE_FILE"
-$BUILDDIR/cooccur -memory $MEMORY -vocab-file $VOCAB_FILE -verbose $VERBOSE -window-size $WINDOW_SIZE < $CORPUS > $COOCCURRENCE_FILE
-echo "$ $BUILDDIR/shuffle -memory $MEMORY -verbose $VERBOSE < $COOCCURRENCE_FILE > $COOCCURRENCE_SHUF_FILE"
-$BUILDDIR/shuffle -memory $MEMORY -verbose $VERBOSE < $COOCCURRENCE_FILE > $COOCCURRENCE_SHUF_FILE
-echo "$ $BUILDDIR/glove -save-file $SAVE_FILE -threads $NUM_THREADS -input-file $COOCCURRENCE_SHUF_FILE -x-max $X_MAX -iter $MAX_ITER -vector-size $VECTOR_SIZE -binary $BINARY -vocab-file $VOCAB_FILE -verbose $VERBOSE"
-$BUILDDIR/glove -save-file $SAVE_FILE -threads $NUM_THREADS -input-file $COOCCURRENCE_SHUF_FILE -x-max $X_MAX -iter $MAX_ITER -vector-size $VECTOR_SIZE -binary $BINARY -vocab-file $VOCAB_FILE -verbose $VERBOSE
+echo "starting running bin files: " | tee -a $MODEL/log.txt
+echo "$ $BUILDDIR/vocab_count -min-count $VOCAB_MIN_COUNT -verbose $VERBOSE < $MODEL/data.txt > $MODEL/vocab.txt" \
+    | tee -a $MODEL/log.txt
+$BUILDDIR/vocab_count -min-count $VOCAB_MIN_COUNT -verbose $VERBOSE < $MODEL/data.txt > $MODEL/vocab.txt
 
-echo "$ $PYTHON eval/python/evaluate.py"
-$PYTHON eval/python/evaluate.py
+echo "$ $BUILDDIR/cooccur -memory $MEMORY -vocab-file $MODEL/vocab.txt -verbose $VERBOSE -window-size $WINDOW_SIZE < $MODEL/data.txt > $MODEL/cooccurrence.bin" | tee -a $MODEL/log.txt
+$BUILDDIR/cooccur -memory $MEMORY -vocab-file $MODEL/vocab.txt -verbose $VERBOSE -window-size $WINDOW_SIZE < $MODEL/data.txt > $MODEL/cooccurrence.bin
+
+echo "$ $BUILDDIR/shuffle -memory $MEMORY -verbose $VERBOSE < $MODEL/cooccurrence.bin > $MODEL/cooccurrence.shuf.bin" \
+    | tee -a $MODEL/log.txt
+$BUILDDIR/shuffle -memory $MEMORY -verbose $VERBOSE < $MODEL/cooccurrence.bin > $MODEL/cooccurrence.shuf.bin
+
+echo "$ $BUILDDIR/glove -save-file $MODEL/vectors -threads $NUM_THREADS -input-file $MODEL/cooccurrence.shuf.bin -x-max $X_MAX -iter $MAX_ITER -vector-size $VECTOR_SIZE -binary $BINARY -vocab-file $MODEL/vocab.txt -verbose $VERBOSE" \
+    | tee -a $MODEL/log.txt
+$BUILDDIR/glove -save-file $MODEL/vectors -threads $NUM_THREADS -input-file $MODEL/cooccurrence.shuf.bin -x-max $X_MAX -iter $MAX_ITER -vector-size $VECTOR_SIZE -binary $BINARY -vocab-file $MODEL/vocab.txt -verbose $VERBOSE
+
+#echo "$ $PYTHON eval/python/evaluate.py" | tee -a $MODEL/log.txt
+#$PYTHON $EMBEDBINFILES"/GloVe/eval/python/evaluate.py" | tee -a $MODEL/log.txt
