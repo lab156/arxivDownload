ad570980955914fd42b3baf099861fa6941c89db
Luis <lab156@case.edu>
2022-12-26 10:24:12 -0500

    organize the mp_queue scripts

--- slurm_scripts/gpu_scripts/mp_queue/mp_queue_task.sh
+++ slurm_scripts/gpu_scripts/mp_queue/mp_queue_task.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=0-3:00:00
 #SBATCH --job-name=class_task
-#SBATCH --output=class_task_w8_log.txt
+#SBATCH --output=class_00_3rd.txt
 #SBATCH --mail-user=lab232@pitt.edu #send email to this address if ...
 #SBATCH --mail-type=END,FAIL # ... job ends or fails
 ##SBATCH --partition=GPU-shared
@@ -18,6 +18,6 @@ singularity run --nv \
     --bind $HOME/arxivDownload:/opt/arxivDownload,$PROJECT:/opt/data_dir \
     $HOME/singul/runner.sif python3 embed/mp_classify.py \
     --model /opt/data_dir/trained_models/lstm_classifier/lstm_Aug-19_17-22 \
-    --out $PROJECT/with_mp_classify3 \
+    --out $PROJECT/with_mp_classify4 \
     --mine /opt/data_dir/promath/math00/*.tar.gz
 cp -r /tmp/trainer $PROJECT/with_mp_classify3/trainer_logs
---
+++ slurm_scripts/gpu_scripts/mp_queue/mp_queue_task_oughts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --time=0-10:00:00
+#SBATCH --job-name=class_01_10
+#SBATCH --output=class_task_w8_log.txt
+#SBATCH --mail-user=lab232@pitt.edu #send email to this address if ...
+#SBATCH --mail-type=END,FAIL # ... job ends or fails
+##SBATCH --partition=GPU-shared
+##SBATCH --gpus=1
+#SBATCH --partition=GPU
+#SBATCH --gpus=8
+
+#source $HOME/env1/bin/activate
+#module load AI/anaconda3-tf2.2020.11
+#source activate $AI_ENV
+
+export TF_CUDNN_RESET_RND_GEN_STATE=1
+singularity run --nv \
+    --bind $HOME/arxivDownload:/opt/arxivDownload,$PROJECT:/opt/data_dir \
+    $HOME/singul/runner.sif python3 embed/mp_classify.py \
+    --model /opt/data_dir/trained_models/lstm_classifier/lstm_Aug-19_17-22 \
+    --out $PROJECT/with_mp_classify3 \
+    --mine /opt/data_dir/promath/math{01..10}/*.tar.gz
+cp -r /tmp/trainer $PROJECT/with_mp_classify3/trainer_logs
