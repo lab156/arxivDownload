d0348d820ec842c6552747ddfa49bb111f460282
Luis <lab156@case.edu>
2023-06-27 16:01:38 -0400

    Tue Jun 27 04:01:38 PM EDT 2023

--- slurm_scripts/gpu_scripts/finetune_train.sh
+++ slurm_scripts/gpu_scripts/finetune_train.sh
@@ -14,7 +14,7 @@
 OUTDIRNAME=finetune
 singularity run --nv \
     --bind $HOME/arxivDownload:/opt/arxivDownload,$PROJECT:/opt/data_dir \
-    $HOME/singul/runnerTransHF.sif python3 embed/fine_tune_classify_HF.py \
+    $PROJECT/singul/runnerTransHF.sif python3 embed/fine_tune_classify_HF.py \
     --model /opt/data_dir/finetuned_models/model_start \
 
 cp -r /tmp/trainer $PROJECT/$OUTDIRNAME/trainer_logs
