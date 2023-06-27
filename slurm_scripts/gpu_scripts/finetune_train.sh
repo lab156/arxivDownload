b85a5b15990c209227293a13a70d5099c39b61a1
Luis <lab156@case.edu>
2023-06-27 15:43:40 -0400

    Tue Jun 27 03:43:40 PM EDT 2023

---
+++ slurm_scripts/gpu_scripts/finetune_train.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --time=1-10:00:00
+#SBATCH --job-name=ft-%j
+#SBATCH --output=finetune-%j.txt
+#SBATCH --mail-user=lab232@pitt.edu #send email to this address if ...
+#SBATCH --mail-type=END,FAIL # ... job ends or fails
+#SBATCH --partition=GPU-shared
+#SBATCH --gpus=1
+##SBATCH --partition=GPU
+##SBATCH --gpus=8
+
+
+#export TF_CUDNN_RESET_RND_GEN_STATE=1
+OUTDIRNAME=finetune
+singularity run --nv \
+    --bind $HOME/arxivDownload:/opt/arxivDownload,$PROJECT:/opt/data_dir \
+    $HOME/singul/runnerTransHF.sif python3 embed/fine_tune_classify_HF.py \
+    --model /opt/data_dir/finetuned_models/model_start \
+
+cp -r /tmp/trainer $PROJECT/$OUTDIRNAME/trainer_logs
+cp ./class_bb_2nd_half.txt $PROJECT/$OUTDIRNAME/trainer_logs
