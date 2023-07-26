0b561f546787a215cbd801c4f5dcabca737cb263
Luis <lab156@case.edu>
2023-07-26 11:27:06 -0400

    Wed Jul 26 11:27:06 AM EDT 2023

--- LLMs/fine_tune_ner_HF.py
+++ LLMs/fine_tune_ner_HF.py
@@ -1,21 +1,33 @@
+import sys
+sys.path = [p for p in sys.path if 'luis' not in p]
 import numpy as np
 import tensorflow as tf
 from datasets import load_dataset, Dataset, DatasetDict
 
 from transformers import (AutoTokenizer,
-                          DataCollatorForTokenClassification,
                           create_optimizer,
                           TFAutoModelForTokenClassification,
                           pipeline,
                          )
 
+try:
+    from transformers import DataCollatorForTokenClassification
+except ImportError:
+    from transformers.data.data_collator import default_data_collator as DataCollatorForTokenClassification
+
 from transformers.keras_callbacks import KerasMetricCallback
 
 import evaluate
 from nltk import word_tokenize
+import toml
+from datetime import datetime as dt
+import logging
 
-import sys, os
-currentdir = os.path.abspath(os.path.curdir)
+import sys, os, inspect
+#currentdir = os.path.abspath(os.path.curdir)
+#parentdir = os.path.dirname(currentdir)
+currentdir = os.path.dirname(
+        os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir) 
 sys.path.insert(0,parentdir+'/embed') 
@@ -26,37 +38,37 @@ import ner.llm_utils as llu
 
 from nltk.chunk import conlltags2tree, ChunkScore
 
-seqeval = evaluate.load('seqeval')
+#seqeval = evaluate.load('seqeval')
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def gen_cfg(**kwargs):
-    # GET the parse args default values
     config_path = kwargs.get('configpath', 'config.toml')
-    cfg = toml.load(config_path)['finetuning']
+    cfg = toml.load(config_path)['finetuning-ner']
     cfg.update(kwargs)
-
-    # This is permanent storage
+    cfg.update({'wiki_src': 'wikipedia/wiki_definitions_improved.xml.gz',
+            'pm_src':  'planetmath/datasets/planetmath_definitions.xml.gz',
+            'stacks_src': 'stacks-project/datasets/stacks-definitions.xml.gz',
+            'punkt_tok_src': 'trained_models/ner_model/oldie/punkt_params.pickle',
+            'glob_data_src': '/train_ner/math*/*.xml.gz',
+            'embed_data_src':  'embeddings/model4ner_19-33_02-01/vectors.bin',
+            })
+    # This is permanent storage# This is permanent storage
     cfg['base_dir'] = os.environ.get('PERMSTORAGE', '/media/hd1') 
-    # This is temporary fast storage
     cfg['local_dir'] = os.environ.get('TEMPFASTSTORAGE',
-            '/tmp/rm_me_finetuning')  
+            '/tmp/rm_me_NER')  # This is temporary fast storage
 
-    # CREATE LOG FILE AND OBJECT
     hoy = dt.now()
     timestamp = hoy.strftime("%b-%d_%H-%M")
-    cfg['timestamp'] = timestamp
-    path_str = 'trained_models/finetuning/HFTransformers_' + timestamp
-    cfg['save_path_dir'] = os.path.join(cfg['base_dir'], path_str)
-    
-    # xml_lst is too long to go in the config
-    xml_lst = glob.glob(
-        os.path.join(cfg['base_dir'], cfg['glob_data_source']))
-    
-	#checkpoint = 'bert-large-cased'
-    
+    cfg['save_path_dir'] = os.path.join(cfg['local_dir'],
+            'trained_ner/lstm_ner/ner_' + timestamp)
+    os.makedirs(cfg['save_path_dir'], exist_ok=True)
+
     FHandler = logging.FileHandler(cfg['local_dir']+"/training.log")
     logger.addHandler(FHandler)
-    
-    return xml_lst, cfg
+
+    return cfg
 
 def parse_args():
     '''
@@ -76,33 +88,67 @@ def parse_args():
 
     return vars(args)
 
-def prepare_ds(cfg):
-	#cfg = tn.gen_cfg()
-	text_lst = tn.get_wiki_pm_stacks_data(cfg)
-	sent_tok, trainer_params = tn.gen_sent_tokzer(text_lst, cfg)
-	tokens_lst, ner_tags_lst, title_lst = ner.bio_tag.put_ner_tags(text_lst, sent_tok)
-	def_lst = ner.bio_tag.put_pos_ner_tags(text_lst, sent_tok)
+def prepare_ds(cfg, model, tokenizer):
+    #cfg = tn.gen_cfg()
+    text_lst = tn.get_wiki_pm_stacks_data(cfg)
+    logger.info(f"The length of text_lst is {len(text_lst)}")
+    sent_tok, trainer_params = tn.gen_sent_tokzer(text_lst, cfg)
+    tokens_lst, ner_tags_lst, title_lst = ner.bio_tag.put_ner_tags(text_lst, sent_tok)
+    def_lst = ner.bio_tag.put_pos_ner_tags(text_lst, sent_tok)
 
-	pos_lst = [[d[0][1] for d in tree_lst['ner']] for tree_lst in def_lst]
-	data_dict = {
-		'id': list(range(len(tokens_lst))),
-		'tokens': tokens_lst,
-		'ner_tags': ner_tags_lst,
-		'title': title_lst,
-		'pos': pos_lst,
-	}
-	ds = Dataset.from_dict(data_dict)
-	temp1_dd = ds.train_test_split(test_size=0.1, shuffle=True)
-	temp2_dd = temp1_dd['train'].train_test_split(test_size=0.1, shuffle=True)
+    pos_lst = [[d[0][1] for d in tree_lst['ner']] for tree_lst in def_lst]
+    data_dict = {
+        'id': list(range(len(tokens_lst))),
+        'tokens': tokens_lst,
+        'ner_tags': ner_tags_lst,
+        'title': title_lst,
+        'pos': pos_lst,
+    }
+    ds = Dataset.from_dict(data_dict)
+    temp1_dd = ds.train_test_split(test_size=0.1, shuffle=True)
+    temp2_dd = temp1_dd['train'].train_test_split(test_size=0.1, shuffle=True)
 
-	ds = DatasetDict({
-		'train': temp2_dd['train'],
-		'test': temp1_dd['test'],
-		'valid': temp2_dd['test'],
-	})
-	return ds
+    ds = DatasetDict({
+        'train': temp2_dd['train'],
+        'test': temp1_dd['test'],
+        'valid': temp2_dd['test'],
+            })
 
-def tokenize_and_align_labels(examples):
+    logger.info(f"{ds=}")
+    tokenized_ds = ds.map(tokenize_and_align_labels, batched=True,
+            fn_kwargs={'tokenizer': tokenizer})
+
+    data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer,
+                     return_tensors="tf")
+
+    tf_train_set = model.prepare_tf_dataset(
+        tokenized_ds["train"],
+        shuffle=True,
+        batch_size=cfg['batch_size'],
+        collate_fn=data_collator,
+    )
+
+    tf_validation_set = model.prepare_tf_dataset(
+        tokenized_ds["valid"],
+        shuffle=False,
+        batch_size=cfg['batch_size'],
+        collate_fn=data_collator,
+    )
+
+    tf_test_set = model.prepare_tf_dataset(
+        tokenized_ds["test"],
+        shuffle=False,
+        batch_size=cfg['batch_size'],
+        collate_fn=data_collator,
+    )
+
+    return (tf_train_set, 
+            tf_validation_set,
+            tf_test_set,
+            ds,
+           )
+
+def tokenize_and_align_labels(examples, tokenizer):
     'Should be run with the dataset.map method'
     tokenized_inputs = tokenizer(examples['tokens'],
                                 truncation=True,
@@ -127,8 +173,12 @@ def tokenize_and_align_labels(examples):
     return tokenized_inputs
 
 def compute_metrics(p):
+    '''
+    module evaluate was not installed in the initial Singularity container
+    '''
     predictions, labels = p
     predictions = np.argmax(predictions, axis=2)
+    label_list = ['O', 'B-DFNDUM', 'I-DFNDUM']
     
     true_predictions = [
         [label_list[p] for (p, l) in zip(prediction, label) if l != -100]
@@ -140,7 +190,7 @@ def compute_metrics(p):
         for prediction, label in zip(predictions, labels)
     ]
     
-    results = seqeval.compute(predictions=true_predictions, references=true_labels)
+    #results = seqeval.compute(predictions=true_predictions, references=true_labels)
     
     return {
         'precision': results['overall_precision'],
@@ -149,106 +199,113 @@ def compute_metrics(p):
         'accuracy': results['overall_accuracy'],
     }
 
-def compute_chunkscore(ds_test, model, cfg):
-	chunkscore = ChunkScore()
 
-	spec_toks = list(tokenizer.special_tokens_map.values())
-	spec_toks.remove('[UNK]')
+def compute_chunkscore(ds_test, model, tokenizer, cfg):
+    '''
+    ds_test: ds['test'] 
+    
+    '''
+    chunkscore = ChunkScore()
+
+    spec_toks = list(tokenizer.special_tokens_map.values())
+    spec_toks.remove('[UNK]')
 
 
-	dif_len_lst = []
-	for j in range(len(ds_test)):
-		tt = tokenizer(ds_test[j]['tokens'], 
-					   return_tensors='tf', is_split_into_words=True)
-		logits = model(**tt).logits
+    dif_len_lst = []
+    for j in range(len(ds_test)):
+        tt = tokenizer(ds_test[j]['tokens'], 
+                       return_tensors='tf', is_split_into_words=True)
+        logits = model(**tt).logits
 
-		# Grouping entities
-		predicted_ids = tf.math.argmax(logits, axis=-1)[0]
-		predictions = predicted_ids.numpy().tolist()
-		pp = [model.config.id2label[t] for t in predictions]
+        # Grouping entities
+        predicted_ids = tf.math.argmax(logits, axis=-1)[0]
+        predictions = predicted_ids.numpy().tolist()
+        pp = [model.config.id2label[t] for t in predictions]
 
-		wl, il = llu.get_words_back(tt.tokens(),
-							  preds=pp, special_tokens=spec_toks)
-		try:
-			wl, il = llu.join_by_example(wl, ds_test[j]['tokens'], preds=il)
-		except AssertionError:
-			print(f'Index {j=} caused the error')
+        wl, il = llu.get_words_back(tt.tokens(),
+                              preds=pp, special_tokens=spec_toks)
+        try:
+            wl, il = llu.join_by_example(wl, ds_test[j]['tokens'], preds=il)
+        except AssertionError:
+            print(f'Index {j=} caused the error')
 
-		tree_pred = conlltags2tree([(tok, 'Upa', pred) for tok,
+        tree_pred = conlltags2tree([(tok, 'Upa', pred) for tok,
                                      pred in zip(wl, il)])
 
-		jdict = ds_test[j]
-		bio_tagged = tn.tf_bio_tagger(jdict['ner_tags'])
-		tree_gold = conlltags2tree([(jdict['tokens'][i], 
-									 'Upa', 
-									 bio_tagged[i])
-									for i in range(len(jdict['tokens']))])
+        jdict = ds_test[j]
+        bio_tagged = tn.tf_bio_tagger(jdict['ner_tags'])
+        tree_gold = conlltags2tree([(jdict['tokens'][i], 
+                                     'Upa', 
+                                     bio_tagged[i])
+                                    for i in range(len(jdict['tokens']))])
 
-		chunkscore.score(tree_pred, tree_gold)
-		
-		if len(wl) != len(jdict['tokens']):
-			dif_len_lst.append(j)
-	print(chunkscore)
+        chunkscore.score(tree_pred, tree_gold)
+        
+        if len(wl) != len(jdict['tokens']):
+            dif_len_lst.append(j)
+    return chunkscore
 
-# get tokenizer
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-tokenized_ds = ds.map(tokenize_and_align_labels, batched=True)
 
-id2label = {
-     0: "O",
-     1: "B-DFNDUM",
-     2: "I-DFNDUM",
- }
- label2id = {
-     "O": 0,
-     "B-DFNDUM": 1,
-     "I-DFNDUM": 2,
- }
+def main():
+    # get tokenizer
 
-cfg['num_train_steps'] = (len(tokenized_ds['train']) // batch_size * num_train_epochs)
-optimizer, lr_schedule = create_optimizer(
-    init_lr = cfg['init_lr'],
-    num_train_steps=cfg['num_train_steps'],
-    weight_decay_rate=cfg['weight_decay_rate'],
-    num_warmup_steps=cfg['num_warmup_steps'],
-)
+    id2label = {
+         0: "O",
+         1: "B-DFNDUM",
+         2: "I-DFNDUM",
+     }
+    label2id = {
+         "O": 0,
+         "B-DFNDUM": 1,
+         "I-DFNDUM": 2,
+     }
 
-model = TFAutoModelForTokenClassification.from_pretrained(
-    cfg['checkpoint'],
-    num_labels=cfg['num_labels'],
-    id2label=id2label,
-    label2id=label2id,
-)
+    args = parse_args()
+    cfg = gen_cfg(**args)
+    print(cfg['num_labels']) 
+
+    xla_gpu_lst = tf.config.list_physical_devices("XLA_GPU")
+    logger.info(f'List of XLA GPUs: {xla_gpu_lst}')
+
+    tokenizer = AutoTokenizer.from_pretrained(cfg['checkpoint'])
     
-data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer, return_tensors="tf")
+    model = TFAutoModelForTokenClassification.from_pretrained(
+        cfg['checkpoint'],
+        num_labels=cfg['num_labels'],
+        id2label=id2label,
+        label2id=label2id,
+    )
+    
+    (tf_train_data, 
+    tf_validation_data,
+    tf_test_data,
+    ds
+    ) = prepare_ds(cfg, model, tokenizer)
 
-tf_train_set = model.prepare_tf_dataset(
-    tokenized_ds["train"],
-    shuffle=True,
-    batch_size=8,
-    collate_fn=data_collator,
-)
+    cfg['num_train_steps'] = (len(ds['train']) // cfg['batch_size'] *
+            cfg['epochs'])
+    optimizer, lr_schedule = create_optimizer(
+        init_lr = cfg['init_lr'],
+        num_train_steps=cfg['num_train_steps'],
+        weight_decay_rate=cfg['weight_decay_rate'],
+        num_warmup_steps=cfg['num_warmup_steps'],
+    )
 
-tf_validation_set = model.prepare_tf_dataset(
-    tokenized_ds["valid"],
-    shuffle=False,
-    batch_size=8,
-    collate_fn=data_collator,
-)
+        
+    model.compile(optimizer=optimizer)
 
-tf_test_set = model.prepare_tf_dataset(
-    tokenized_ds["test"],
-    shuffle=False,
-    batch_size=8,
-    collate_fn=data_collator,
-)
+    metric_callback = KerasMetricCallback(metric_fn=compute_metrics,
+            eval_dataset=tf_validation_data)
+    callbacks = [metric_callback,]
+    model.fit(x=tf_train_data,
+              validation_data=tf_validation_data,
+              epochs=cfg['epochs'])
+              #callbacks=callbacks)
 
-model.compile(optimizer=optimizer)
+    chscore = compute_chunkscore(ds['test'], model, tokenizer, cfg)
+    print(chscore)
+    logger.info(chscore)
+    logger.info(f"{cfg=}")
 
-metric_callback = KerasMetricCallback(metric_fn=compute_metrics, eval_dataset=tf_validation_set)
-callbacks = [metric_callback,]
-model.fit(x=tf_train_set,
-          validation_data=tf_validation_set,
-          epochs=3,
-          callbacks=callbacks)
-
+if __name__ == "__main__":
+    main()
