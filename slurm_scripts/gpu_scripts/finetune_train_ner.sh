61551fa1dda8bb6e7328fb83021a561a2d43e057
Luis <lab156@case.edu>
2023-07-31 15:44:12 -0400

    Mon Jul 31 03:44:12 PM EDT 2023

---
+++ slurm_scripts/gpu_scripts/finetune_train_ner.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --time=0-10:00:00
+#SBATCH --job-name=finetuning
+#SBATCH --output=finetune_ner.txt
+#SBATCH --mail-user=lab232@pitt.edu #send email to this address if ...
+#SBATCH --mail-type=END,FAIL # ... job ends or fails
+#SBATCH --partition=GPU-shared
+#SBATCH --gpus=1
+##SBATCH --partition=GPU
+##SBATCH --gpus=8
+
+
+#export TF_CUDNN_RESET_RND_GEN_STATE=1
+OUTDIRNAME="finetune_ner/class-"$(date "+%Y-%m-%d_%H%M")
+singularity run --nv \
+    --bind $HOME/arxivDownload:/opt/arxivDownload,$PROJECT:/opt/data_dir \
+    $PROJECT/singul/runnerNERNew.sif python3 LLMs/fine_tune_ner_HF.py \
+    --configpath /opt/arxivDownload/config.toml
+
+#mkdir $PROJECT/$OUTDIRNAME/
+cp -r /tmp/trainer $PROJECT/$OUTDIRNAME/trainer_logs
+cp ./finetune_ner.txt $PROJECT/$OUTDIRNAME/trainer_logs
+
