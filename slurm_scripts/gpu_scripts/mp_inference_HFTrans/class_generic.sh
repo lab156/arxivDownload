5a7049f34d80501a8930606eef8802354fd22c9a
Luis <lab156@case.edu>
2023-08-17 22:55:15 -0400

    Thu Aug 17 10:55:15 PM EDT 2023

--- slurm_scripts/gpu_scripts/mp_inference_HFTrans/class_generic.sh
+++ slurm_scripts/gpu_scripts/mp_inference_HFTrans/class_generic.sh
@@ -9,17 +9,20 @@
 #SBATCH --partition=GPU
 #SBATCH --gpus=8
 
-#source $HOME/env1/bin/activate
-#module load AI/anaconda3-tf2.2020.11
-#source activate $AI_ENV
+# Change Parameters and Variables
+# --job-name
+# --output (change also in the last line)
+# --OUTDIRNAME
+# maybe: --mine
+
 
 export TF_CUDNN_RESET_RND_GEN_STATE=1
-OUTDIRNAME=HF_test
+OUTDIRNAME=HF_generic
 singularity run --nv \
     --bind $HOME/arxivDownload:/opt/arxivDownload,$PROJECT:/opt/data_dir \
     $PROJECT/singul/runnerTransHF.sif python3 LLMs/mp_HF_classify_inference.py \
     --model /ocean/projects/mth220001p/lab232/finetune/class-2023-08-07_1327 \
     --out $PROJECT/$OUTDIRNAME \
-    --mine /opt/data_dir/promath/math99/*.tar.gz
+    --mine /opt/data_dir/promath/math0{0,1}/*_00{1,2,3,4}.tar.gz
 cp -r /tmp/trainer $PROJECT/$OUTDIRNAME/trainer_logs
 cp ./class_test-mp.txt $PROJECT/$OUTDIRNAME/trainer_logs
