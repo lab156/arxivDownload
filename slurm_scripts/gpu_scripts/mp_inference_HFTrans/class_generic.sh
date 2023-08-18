065454eb35f294b0e5725e63ba872f2ac37b8f6e
Luis <lab156@case.edu>
2023-08-18 00:18:43 -0400

    Fri Aug 18 12:18:43 AM EDT 2023

--- slurm_scripts/gpu_scripts/mp_inference_HFTrans/class_generic.sh
+++ slurm_scripts/gpu_scripts/mp_inference_HFTrans/class_generic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --time=0-02:00:00
-#SBATCH --job-name=c_test-mp
-#SBATCH --output=class-mp.txt
+#SBATCH --time=0-20:00:00
+#SBATCH --job-name=2nd-half
+#SBATCH --output=infer-2nd-half.txt
 #SBATCH --mail-user=lab232@pitt.edu #send email to this address if ...
 #SBATCH --mail-type=END,FAIL # ... job ends or fails
 ##SBATCH --partition=GPU-shared
@@ -10,6 +10,7 @@
 #SBATCH --gpus=8
 
 # Change Parameters and Variables
+# --time
 # --job-name
 # --output (change also in the last line)
 # --OUTDIRNAME
@@ -17,12 +18,12 @@
 
 
 export TF_CUDNN_RESET_RND_GEN_STATE=1
-OUTDIRNAME=HF_generic
+OUTDIRNAME=infer_2nd_half
 singularity run --nv \
     --bind $HOME/arxivDownload:/opt/arxivDownload,$PROJECT:/opt/data_dir \
     $PROJECT/singul/runnerTransHF.sif python3 LLMs/mp_HF_classify_inference.py \
     --model /ocean/projects/mth220001p/lab232/finetune/class-2023-08-07_1327 \
     --out $PROJECT/$OUTDIRNAME \
-    --mine /opt/data_dir/promath/math0{0,1}/*_00{1,2,3,4}.tar.gz
+    --mine /opt/data_dir/promath/math{15,16,17,18,19,20}/*.tar.gz
 cp -r /tmp/trainer $PROJECT/$OUTDIRNAME/trainer_logs
-cp ./class-mp.txt $PROJECT/$OUTDIRNAME/trainer_logs
+cp ./infer-2nd-half.txt $PROJECT/$OUTDIRNAME/trainer_logs
