6a9aca8c0bd809ebbc057a367494a33472042e92
Luis <lab156@case.edu>
2023-11-01 23:26:17 -0400

    Wed Nov  1 11:26:17 PM EDT 2023

---
+++ interact_scripts/finetune_pytorch_gpt2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# To be run in interact mode potentially with multiple gpus
+
+singularity run --nv \
+    --bind $HOME/arxivDownload:/opt/arxivDownload,$PROJECT:/opt/data_dir \
+    $PROJECT/singul/runnerTransHF.sif python3 LLMs/finetune_class_HF_pytorch.py \
+    --savedir /opt/data_dir/result/model \
+    --configpath /opt/arxivDownload/rmme_config.toml
