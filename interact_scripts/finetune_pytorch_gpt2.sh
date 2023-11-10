68a444929268801321bdd9436ef632074b3bedb2
Luis <lab156@case.edu>
2023-11-10 12:43:37 -0500

    another try

--- interact_scripts/finetune_pytorch_gpt2.sh
+++ interact_scripts/finetune_pytorch_gpt2.sh
@@ -2,11 +2,11 @@
 
 # To be run in interact mode potentially with multiple gpus
 
+export TRANSFORMERS_CACHE=$PROJECT/hfcache
 singularity run --nv \
     --bind $HOME/arxivDownload:/opt/arxivDownload,$PROJECT:/opt/data_dir \
     $PROJECT/singul/runnerPytorchTransHF.sif python3 \
     LLMs/finetune_class_HF_pytorch.py \
     --savedir /opt/data_dir/result/model \
     --configpath /opt/arxivDownload/rmme_config.toml \
-    --hfpath $PROJECT/hfcache
 
