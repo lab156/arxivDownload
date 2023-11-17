bd2da6d4974fe7b21567ebed57c5bc33c941a11e
Luis <lab156@case.edu>
2023-11-16 21:53:45 -0500

    Thu Nov 16 09:53:45 PM EST 2023

--- slurm_scripts/gpu_scripts/multi_gpu_finetune_pytorch.sh
+++ slurm_scripts/gpu_scripts/multi_gpu_finetune_pytorch.sh
@@ -4,8 +4,8 @@
 #SBATCH --output=class_ft-%j.txt
 #SBATCH --mail-user=lab232@pitt.edu #send email to this address if ...
 #SBATCH --mail-type=END,FAIL # ... job ends or fails
-#SBATCH --partition=GPU
-#SBATCH --gpus=8
+#SBATCH --partition=GPU-shared
+#SBATCH --gpus=v100-32:4
 
 # To be run in interact mode potentially with multiple gpus
 OUTDIRNAME="finetune/class-"$(date "+%Y-%m-%d_%H%M")
