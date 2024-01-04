4507b14d1f437da9553a64e328edfd954470c21b
Luis <lab156@case.edu>
2024-01-04 17:11:57 -0500

    Thu Jan  4 05:11:57 PM EST 2024

---
+++ slurm_scripts/cerebras_scripts/salloc.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+srun --pty --cpus-per-task=28 --kill-on-bad-exit \
+    singularity shell --cleanenv --bind \
+    /local1/cerebras/data,/local2/cerebras/data,\
+    /local3/cerebras/data,/local4/cerebras/data,$PROJECT \
+    /ocean/neocortex/cerebras/cbcore_latest.sif
