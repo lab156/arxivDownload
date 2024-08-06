4a36ea753b781c048798d73699f3af0b71bb7dd7
Luis <lab156@case.edu>
2024-08-06 12:06:24 -0600

    Tue Aug  6 12:06:24 PM CST 2024

--- LLMs/small_ner_infer_HFT.py
+++ LLMs/small_ner_infer_HFT.py
@@ -1,7 +1,15 @@
 import re
 import os
 from os.path import join
+import numpy as np
 
+from transformers import (AutoTokenizer,
+                          create_optimizer,
+                          TFAutoModelForTokenClassification,
+                          pipeline,
+                         )
+
+from transformers import DataCollatorForTokenClassification
 
 def parse_args():
     '''
@@ -13,6 +21,9 @@ def parse_args():
     #    help="""Path to save the finetuned model, dir name only.""")
     parser.add_argument('--xdefs', type=str, default='',
         help="""path to the JUrban/extract-defs cloned repository""")
+    parser.add_argument('--model', type=str,
+            default='/home/luis/ner_model',
+            help='Path to the tensorflow model directory')
     args = parser.parse_args()
 
     # make sure --savepath exists
@@ -32,6 +43,77 @@ def get_text(in_str):
     Defin = reg_expr2.findall(in_str)
     return Defin
 
+
+def sanity_check(model, tokenizer, text=None):
+    if text == None:
+        text = """We define a Banach space as a complete vector normed space."""
+    #text = ''
+    #j = 19
+    #for t in ds['test'][j]['tokens']:
+    #    text += t + ' '
+    print(f'{text=}')
+    classifier = pipeline('ner', model=model, tokenizer=tokenizer)
+    print('The pipeline result is: ', classifier(text))
+
+    inputs = tokenizer(text, return_tensors='tf')
+    logits = model(**inputs).logits
+    predicted_ids = tf.math.argmax(logits, axis=-1)
+    predicted_token_class = [model.config.id2label[t] 
+                 for t in predicted_ids[0].numpy().tolist()]
+
+    for i in range(len(predicted_token_class)):
+        print(inputs.tokens()[i], predicted_token_class[i])
+
+    #tt = tokenizer(ds['test'][j]['tokens'], return_tensors='tf', is_split_into_words=True)
+    tt = tokenizer(text, return_tensors='tf', is_split_into_words=False)
+    logits = model(**tt).logits
+    print(f"{logits=}")
+
+    # Grouping entities
+    predicted_ids = tf.math.argmax(logits, axis=-1)[0]
+    predictions = predicted_ids.numpy().tolist()
+    results = []
+    inputs_with_offsets = tokenizer(text, return_offsets_mapping=True)
+    tokens = inputs_with_offsets.tokens()
+    offsets = inputs_with_offsets['offset_mapping']
+
+    probs = tf.math.softmax(logits, axis=-1)[0]
+    probs = probs.numpy().tolist()
+
+    #start, end = inputs.word_to_chars(10)
+    end = 0
+
+    idx = 0
+    while idx < len(predictions):
+        pred = predictions[idx]
+        label = model.config.id2label[pred]
+        if label != 'O':
+            label = label[2:]
+            start, end = offsets[idx] # 2nd output is the end of word
+            #idx += 1
+            
+            # Grab all tokens labeled with an I-label
+            all_scores = []
+            while (
+                idx < len(predictions)
+                and model.config.id2label[predictions[idx]][2:] == label
+                   ):
+                all_scores.append(probs[idx][pred])
+                _, end = offsets[idx]
+                idx += 1
+                
+            score = np.mean(all_scores).item()
+            word = text[start:end]
+            results.append(
+                {'entity': label, 
+                 'score': score,
+                 'word': word,
+                'start': start,
+                'end': end,}
+            )
+        idx += 1
+    print(results)
+
 def main():
     args = parse_args()
     
@@ -49,8 +131,21 @@ def main():
         xdefs_out_lst = fobj.readlines()
     with open(join(xdefs_inputs, xdefs_inputs_filelst[0]), 'r') as fobj:
         xdefs_in_lst = fobj.readlines()
+
+    # LLM loading and preparation
+    tokenizer = AutoTokenizer\
+            .from_pretrained(cfg['checkpoint'])
         
     print(get_text(xdefs_in_lst[15]) )
+    tf_model_dir = args.model
+    cfg = {'outdir': args.out,
+            'max_length': 150, 
+            'inference_batch_size': 250}
+    #with open(os.path.join(tf_model_dir, 'config.json')) as fobj:
+    #    cfg['checkpoint'] = json.loads(fobj.read())['_name_or_path']
+    Model = TFAutoModelForTokenClassification\
+            .from_pretrained(tf_model_dir)
+    sanity_check(Model, tokenizer)
 
 if __name__ == "__main__":
     main()
